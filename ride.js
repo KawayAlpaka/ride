9ea86591552ecae3893353c756fed52f5b6ae51b
L <my917@qq.com>
2016-12-02 16:26:58 +0800

    add temp dir to static

--- ride.js
+++ ride.js
@@ -29,6 +29,7 @@ mongoose.connection.on('connected', function () {
 
 
     app.use(express.static('www'));
+    app.use(express.static('temp'));
     app.use(cookieParser());
     app.use(bodyParser.json({limit: '1mb'}));  //body-parser 解析json格式数据
     app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
